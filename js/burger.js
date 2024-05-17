47ce4960739c49ba8c03c26d1566444be019333f
Laguta Roman <102230980+LagutaGit@users.noreply.github.com>
2024-05-17 14:05:36 +0900

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -9,6 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // Код всплывающей подсказки
-/* document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     alert('Добрый день! Буду честен, некоторые моменты были реализованы немного не так как в макете, но я постарался сделать так, чтобы подходило по стилистике сайта. Спасибо за внимание! P.S. Если для проверки данная надпись будет мешать, код от неё находится в файле burger.js');
-}); */
+}); 
